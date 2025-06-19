9b33494d647a203d2935e5fe89c64237cd6ae51c
MIR1951 <160203220+MIR1951@users.noreply.github.com>
2025-06-19 13:05:27 +0500

    Initial commit

---
+++ FlashCard/FlashCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardApp.swift
+//  FlashCard
+//
+//  Created by Kenjaboy Xajiyev on 19/06/25.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlashCardTests/FlashCardTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardTests.swift
+//  FlashCardTests
+//
+//  Created by Kenjaboy Xajiyev on 19/06/25.
+//
+
+import Testing
+@testable import FlashCard
+
+struct FlashCardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
